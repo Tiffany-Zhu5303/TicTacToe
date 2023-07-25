e521429d9a2d0fec05ba111d9df12d6bd4b87a35
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-07-25 19:45:46 -0400

    Centered and colored

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Square({value, onSquareClick}){
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9). fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
     if(squares[i] || calculateWinner(squares)){
@@ -37,7 +37,7 @@ export default function Board() {
   return (
     <>
     <div className="status">{status}</div>
-    <div className="board-row">
+    <div className="board-row top-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
       <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
